9b1c3586ff481d4fba791fb94f0eaf43c46fe809
VIVEK KUMAR VERMA <vverma508@gmail.com>
2017-09-03 16:45:11 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -2,10 +2,10 @@ exports.showLocationGallary = function(req, res) {
     if (req.query.loc == 'goa') {
         res.render('goa.ejs', {});
     } else {
-        res.render('nandiHill.ejs', {});
+        res.render('nandiHill', {});
     }
 };
 
 exports.welcomePage = function(req, res) {
-    res.render('welcome.ejs', {});
+    res.render('welcome', {});
 }
